63d470e3988feee14abb4005f740c714e2a0a1f8
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2019-07-25 04:59:17 +0200

    Minor update

--- NanoAODAnalyzer/python/RunNanoSkimmer_Local.py
+++ NanoAODAnalyzer/python/RunNanoSkimmer_Local.py
@@ -7,18 +7,18 @@ from XMuMuAnalysis.NanoAODAnalyzer.postproc.XMuMuNanoSkimmer import XMuMuNanoSki
 from XMuMuAnalysis.NanoAODAnalyzer.postproc.XMuMuNanoSkimmer import XMuMuNanoSkimmer_2016_data
 
 CMSSW_BASE  = os.environ['CMSSW_BASE']
-# CMSXROOTD   = "root://xrootd-cms.infn.it/"
-CMSXROOTD   = "root://cms-xrd-global.cern.ch"
+CMSXROOTD   = "root://xrootd-cms.infn.it/"
+# CMSXROOTD   = "root://cms-xrd-global.cern.ch"
 
 isMC=True
 
-# sampleDir = CMSXROOTD
-# sampleDir += "/store/mc/NANOAODSIM/PUMoriond17_Nano14Dec2018_102X_mcRun2_asymptotic_v6-v1"
-# sampleDir += "/RunIISummer16NanoAODv4/MSSMggA-HiggsToMuMu_MA-400_Tanb-30_13TeV_pythia8/100000/"
-# infile = [sampleDir+"B7225465-4A05-1246-BC7E-B67019BB7ECE.root"]
+sampleDir = CMSXROOTD
+sampleDir += "/store/mc/NANOAODSIM/PUMoriond17_Nano14Dec2018_102X_mcRun2_asymptotic_v6-v1"
+sampleDir += "/RunIISummer16NanoAODv4/MSSMggA-HiggsToMuMu_MA-400_Tanb-30_13TeV_pythia8/100000/"
+infile = [sampleDir+"B7225465-4A05-1246-BC7E-B67019BB7ECE.root"]
 
-infile = ["C5D5AF70-6B15-6746-8DD5-E20B1A5609B2.root"]
-isMC=True
+# infile = ["C5D5AF70-6B15-6746-8DD5-E20B1A5609B2.root"]
+# isMC=True
 
 # infile = ["152878C4-A061-9048-9739-F46D95A2D975.root"]
 # isMC=False
