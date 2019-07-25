ee830549463b6610707b3d8bc6bd31d04799d3f1
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2019-07-25 04:58:43 +0200

    First minimal local running working version

---
+++ NanoAODAnalyzer/python/RunNanoSkimmer_Local.py
@@ -0,0 +1,65 @@
+import os,sys
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+
+from PhysicsTools.NanoAODTools.postprocessing.framework.postprocessor import * 
+from XMuMuAnalysis.NanoAODAnalyzer.postproc.XMuMuNanoSkimmer import XMuMuNanoSkimmer_2016_mc
+from XMuMuAnalysis.NanoAODAnalyzer.postproc.XMuMuNanoSkimmer import XMuMuNanoSkimmer_2016_data
+
+CMSSW_BASE  = os.environ['CMSSW_BASE']
+# CMSXROOTD   = "root://xrootd-cms.infn.it/"
+CMSXROOTD   = "root://cms-xrd-global.cern.ch"
+
+isMC=True
+
+# sampleDir = CMSXROOTD
+# sampleDir += "/store/mc/NANOAODSIM/PUMoriond17_Nano14Dec2018_102X_mcRun2_asymptotic_v6-v1"
+# sampleDir += "/RunIISummer16NanoAODv4/MSSMggA-HiggsToMuMu_MA-400_Tanb-30_13TeV_pythia8/100000/"
+# infile = [sampleDir+"B7225465-4A05-1246-BC7E-B67019BB7ECE.root"]
+
+infile = ["C5D5AF70-6B15-6746-8DD5-E20B1A5609B2.root"]
+isMC=True
+
+# infile = ["152878C4-A061-9048-9739-F46D95A2D975.root"]
+# isMC=False
+
+
+if len(sys.argv)>2: 
+  outputDir = sys.argv[2]
+else:
+  if isMC: 
+    outputDir = "outputMC" 
+  else:
+    outputDir = "outputData" 
+
+varTxtFileDir = CMSSW_BASE+"/src/XMuMuAnalysis/NanoAODAnalyzer/scripts/"
+varTxtFileIn  = varTxtFileDir+"keep_and_drop_branches_input.txt"
+varTxtFileOut = varTxtFileDir+"keep_and_drop_branches_output.txt"
+
+if isMC:
+  p=PostProcessor(
+    outputDir, 
+    infile,
+    cut="",
+    branchsel=varTxtFileIn,
+    outputbranchsel=varTxtFileOut,
+    modules=[XMuMuNanoSkimmer_2016_mc()],
+    provenance=False,
+    fwkJobReport=False,
+    histFileName="histo.root",
+    histDirName="cutflow"
+  )
+else:
+  p=PostProcessor(
+    outputDir, 
+    infile,
+    cut="",
+    branchsel=varTxtFileIn,
+    outputbranchsel=varTxtFileOut,
+    modules=[XMuMuNanoSkimmer_2016_data()],
+    provenance=False,
+    fwkJobReport=False,
+    histFileName="histo.root",
+    histDirName="cutflow"
+  )
+p.run()
---
+++ NanoAODAnalyzer/python/postproc/XMuMuNanoSkimmer.py
@@ -0,0 +1,86 @@
+import ROOT
+ROOT.PyConfig.IgnoreCommandLineOptions = True
+import sys
+
+from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection,Object
+from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
+from PhysicsTools.NanoAODTools.postprocessing.tools import * #deltaR, matching etc..
+
+class XMuMuNanoSkimmer(Module):
+  def __init__(self, isMC, era, doSkim=False, useCMVA=False):
+    self.era = era
+    self.isMC = isMC
+    self.doSkim = doSkim
+    self.useCMVA = useCMVA
+    self.genJets = []
+    self.writeHistFile=True
+
+  def beginJob(self,histFile=None,histDirName=None):
+    Module.beginJob(self,histFile,histDirName)
+    self.h_cutflow_uw = ROOT.TH1F('h_cutflow_uw','h_cutflow_uw', 6, 0, 6)
+    self.h_cutflow_w  = ROOT.TH1F('h_cutflow_w', 'h_cutflow_w',  6, 0, 6)
+    self.h_cutflow_uw.GetXaxis().SetBinLabel(1,"Cut0:NoSelection")
+    self.h_cutflow_uw.GetXaxis().SetBinLabel(2,"Cut1:>=1LooseMuon")
+    self.h_cutflow_w.GetXaxis().SetBinLabel(1,"Cut0:NoSelection")
+    self.h_cutflow_w.GetXaxis().SetBinLabel(2,"Cut1:>=1LooseMuon")
+
+    self.addObject(self.h_cutflow_uw)
+    self.addObject(self.h_cutflow_w)
+  
+  def endJob(self):
+    Module.endJob(self)
+
+  def beginFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+    pass
+
+  def endFile(self, inputFile, outputFile, inputTree, wrappedOutputTree):
+    pass
+
+  def RegisterCut(self, cutName, weight):
+    self.h_cutflow_uw.Fill(cutName,1)
+    self.h_cutflow_w.Fill(cutName, weight)
+
+  def analyze(self, event):
+    """process event, return True (go to next module) or False (fail, go to next event)"""
+    muons     = list(Collection(event, "Muon"))
+
+    evtweight = 1.0
+    if self.isMC:
+      evtweight = event.genWeight
+
+    #################################################
+    # Register cut before ANY selection
+    ##################################################
+    self.RegisterCut("Cut0:NoSelection",evtweight)
+
+    #################################################
+    #
+    # Muon selection
+    #
+    ##################################################
+    #
+    # Select muons
+    #
+    muonsLoose = []
+    for mu in muons:
+      #
+      # Loose selection
+      #
+      if mu.pt < 20. or abs(mu.eta) > 2.4: continue
+      if mu.mediumId is False: continue
+      muonsLoose.append(mu) 
+    #
+    # CHECK: AT LEAST 1 Loose muon.
+    # Skip event if doSkim=True
+    #
+    if len(muonsLoose) < 1:
+      if self.doSkim: 
+        return False
+    else:
+      self.RegisterCut("Cut1:>=1LooseMuon",evtweight)
+
+    return True
+
+# define modules using the syntax 'name = lambda : constructor' to avoid having them loaded when not needed
+XMuMuNanoSkimmer_2016_mc   = lambda : XMuMuNanoSkimmer(isMC=True, era="2016",doSkim=False) 
+XMuMuNanoSkimmer_2016_data = lambda : XMuMuNanoSkimmer(isMC=False,era="2016",doSkim=True) 
