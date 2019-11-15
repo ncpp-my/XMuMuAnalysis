7128a428aad40a923770c531b222ceae8239523e
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2019-11-15 08:22:34 +0100

    Add helper file

---
+++ NanoAODAnalyzer/scripts/CheckJobStatus.py
@@ -0,0 +1,43 @@
+import os 
+import glob
+
+
+allOutFile =  glob.glob('./Ntuple*.*.out')
+allOutFile.sort() # sort by name
+
+jobsDone=[]
+jobsNotDone=[]
+
+for fileName in allOutFile:
+  file = open(fileName,"r")
+
+
+  lines = file.read()
+  if "ProcessNanoAOD_XZVllqq::DONE" in lines:
+    jobsDone.append(fileName)
+  else:
+    jobsNotDone.append(fileName)
+
+print "============================================"
+print "                Jobs Done                   "
+print "============================================"
+
+for jobs in jobsDone:
+  print jobs
+
+print "============================================"
+print "                Jobs Still Not Done         "
+print "============================================"
+
+for jobs in jobsNotDone:
+  print jobs
+
+
+print "========================================================"
+print "                Checking last line of jobs  not done"
+print "========================================================"
+
+for jobs in jobsNotDone:
+  print jobs 
+  os.system('tail -n 1 ' + jobs)
+  print "\n"
