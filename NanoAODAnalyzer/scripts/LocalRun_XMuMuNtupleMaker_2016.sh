59de0685ebb2c75113764f4170fdd764772a2529
Nurfikri Norjoharuddeen <nurfikri89@gmail.com>
2019-11-15 08:29:01 +0100

    Update batch script

--- NanoAODAnalyzer/scripts/LocalRun_XMuMuNtupleMaker_2016.sh
+++ NanoAODAnalyzer/scripts/LocalRun_XMuMuNtupleMaker_2016.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-
 SAMPLES=(
 "MC16_TTJets_DiLept"
-# "MC16_DYLL"
+"MC16_TT_2L"
+"MC16_WZTo2L2Q"
+"MC16_ZZTo2L2Q"
+"MC16_ZZTo2L2Nu"
+"MC16_ZZTo4L"
+"MC16_WZTo3LNu"
+"MC16_WWTo2L2Nu"
 )
 
 CONFIG="XMuMuAnalysis/NanoAODAnalyzer/scripts/ProcNanoAODSkim_MC2016_cfg.py"
@@ -17,23 +22,23 @@ for SAMPLE in "${SAMPLES[@]}"; do
   ProcessNanoAOD_XMuMu ${CONFIG} ${SAMPLENAME} ${SAMPLENAMEFORXSEC}
 done
 
-# SAMPLES=(
-# "Data16B_SingleMuon"
-# "Data16C_SingleMuon"
-# "Data16D_SingleMuon"
-# "Data16E_SingleMuon"
-# "Data16F_SingleMuon"
-# "Data16G_SingleMuon"
-# "Data16H_SingleMuon"
-# )
+SAMPLES=(
+"Data16B_SingleMuon"
+"Data16C_SingleMuon"
+"Data16D_SingleMuon"
+"Data16E_SingleMuon"
+"Data16F_SingleMuon"
+"Data16G_SingleMuon"
+"Data16H_SingleMuon"
+)
 
-# CONFIG="XMuMuAnalysis/NanoAODAnalyzer/scripts/ProcNanoAODSkim_Data2016_cfg.py"
+CONFIG="XMuMuAnalysis/NanoAODAnalyzer/scripts/ProcNanoAODSkim_Data2016_cfg.py"
 
-# for SAMPLE in "${SAMPLES[@]}"; do
-#   SAMPLENAME="$SAMPLE"
-#   SAMPLENAMEFORXSEC="$SAMPLE"
-#   # 
-#   # Run ProcesssNanoAOD
-#   #
-#   ProcessNanoAOD_XMuMu ${CONFIG} ${SAMPLENAME} ${SAMPLENAMEFORXSEC}
-# done
+for SAMPLE in "${SAMPLES[@]}"; do
+  SAMPLENAME="$SAMPLE"
+  SAMPLENAMEFORXSEC="$SAMPLE"
+  # 
+  # Run ProcesssNanoAOD
+  #
+  ProcessNanoAOD_XMuMu ${CONFIG} ${SAMPLENAME} ${SAMPLENAMEFORXSEC}
+done
